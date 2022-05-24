c9097f4ccf7999e38c087e6510705ad4e6d4b11a
jadnw <jadenwu137@protonmail.com>
2022-05-24 21:10:39 +0700

    :sparkles: Underline hover link

--- src/components/mdx/Anchor.tsx
+++ src/components/mdx/Anchor.tsx
@@ -1,5 +1,5 @@
 const Anchor = ({ ...props }) => (
-  <a className="text-blue-600 dark:text-blue-300" {...props} />
+  <a className="text-blue-600 dark:text-blue-300 hover:underline" {...props} />
 )
 
 export default Anchor
