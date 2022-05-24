ad9c865684834ed39653f2b59ed7825edc2477d7
jadnw <jadenwu137@protonmail.com>
2022-05-24 09:48:53 +0700

    :hammer: Decorate Pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,32 +13,36 @@ const Pagination = ({
   pagination: { page, totalPages },
 }: PaginationProps) => {
   return (
-    <div className="flex items-center justify-between text-sm font-semibold">
-      <Link href={`/${route}/page/${page > 1 ? page - 1 : 1}`}>
-        <a
-          className={`${
-            page > 1
-              ? 'text-primary-500'
-              : 'text-ink-400 line-through pointer-events-none'
-          }`}
-        >
-          Prev
-        </a>
-      </Link>
-      <span>
-        Page {page} of {totalPages}
-      </span>
-      <Link href={`/${route}/page/${page < totalPages ? page + 1 : page}`}>
-        <a
-          className={`${
-            page < totalPages
-              ? 'text-primary-500'
-              : 'text-ink-400 line-through pointer-events-none'
-          }`}
-        >
-          Next
-        </a>
-      </Link>
+    <div className="flex flex-col">
+      <span className="block w-1/2 h-px bg-ink-100 dark:bg-ink-700"></span>
+      <div className="py-2 flex items-center justify-between text-sm font-semibold">
+        <Link href={`/${route}/page/${page > 1 ? page - 1 : 1}`}>
+          <a
+            className={`${
+              page > 1
+                ? 'text-primary-500'
+                : 'text-ink-400 line-through pointer-events-none'
+            }`}
+          >
+            Prev
+          </a>
+        </Link>
+        <span>
+          Page {page} of {totalPages}
+        </span>
+        <Link href={`/${route}/page/${page < totalPages ? page + 1 : page}`}>
+          <a
+            className={`${
+              page < totalPages
+                ? 'text-primary-500'
+                : 'text-ink-400 line-through pointer-events-none'
+            }`}
+          >
+            Next
+          </a>
+        </Link>
+      </div>
+      <span className="self-end block w-1/2 h-px bg-ink-100 dark:bg-ink-700"></span>
     </div>
   )
 }
