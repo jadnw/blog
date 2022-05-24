d3e002583536dcf2efc786d13242344159add5d8
jadnw <jadenwu137@protonmail.com>
2022-05-24 21:06:14 +0700

    :sparkles: Add Ul

--- src/components/MdxRenderer.tsx
+++ src/components/MdxRenderer.tsx
@@ -4,13 +4,15 @@ import { getMDXComponent } from 'mdx-bundler/client'
 import Pre from '@/components/mdx/Pre'
 import Paragraph from '@/components/mdx/Paragraph'
 import Typo from '@/components/mdx/Typo'
+import Ul from '@/components/mdx/Ul'
 
 const components = {
   h1: (props: any) => <Typo as="h1" {...props} />,
   h2: (props: any) => <Typo as="h2" {...props} />,
   h3: (props: any) => <Typo as="h3" {...props} />,
   p: Paragraph,
-  pre: (props: any) => <Pre {...props} />,
+  ul: Ul,
+  pre: Pre,
 }
 
 const MdxRenderer = ({ code }: { code: string }) => {
---
+++ src/components/mdx/Ul.tsx
@@ -0,0 +1,3 @@
+const Ul = ({ ...props }) => <ul className="list-disc list-inside" {...props} />
+
+export default Ul
