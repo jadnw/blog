b12272ef1e74fb9899137b2fde7c9e1dbcf0be27
jadnw <jadenwu137@protonmail.com>
2022-05-24 22:28:23 +0700

    :hammer: Add Tag Page without pagination

---
+++ src/pages/tags/[tag].tsx
@@ -0,0 +1,51 @@
+import type {
+  NextPage,
+  GetStaticProps,
+  GetStaticPaths,
+  InferGetStaticPropsType,
+} from 'next'
+
+import PostListLayout from '@/layouts/PostListLayout'
+
+import { getPostTags, getMdxFrontmattersByTag } from '@/lib/mdx'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const tags = await getPostTags()
+  const labels = tags.map(({ label }) => label)
+
+  return {
+    paths: labels.map((tag) => ({ params: { tag } })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { totalPages, frontmatters } = await getMdxFrontmattersByTag(
+    context.params!.tag as string,
+  )
+
+  return {
+    props: {
+      tag: context.params!.tag,
+      pagination: {
+        page: 1,
+        totalPages,
+      },
+      frontmatters,
+    },
+  }
+}
+
+const Tag: NextPage = ({
+  tag,
+  pagination,
+  frontmatters,
+}: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <PostListLayout
+    title={`Tag: ${tag}`}
+    pagination={pagination}
+    frontmatters={frontmatters}
+  />
+)
+
+export default Tag
