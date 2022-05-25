2ba3b2d12aaef4170169a324cb7ad4d5e159b1c9
jadnw <jadenwu137@protonmail.com>
2022-05-25 21:35:02 +0700

    :iphone: Responsive SearchField

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -9,7 +9,7 @@ interface SearchFieldProps {
 const SearchField = ({ value, placeholder, handler }: SearchFieldProps) => (
   <div className="pr-4 w-full sm:w-96 flex items-center bg-white dark:bg-ink-800 border border-ink-100 dark:border-ink-700 rounded-sm">
     <input
-      className="flex-1 px-4 py-2 text-sm bg-white dark:bg-ink-800 outline-none"
+      className="flex-1 px-4 py-1 xs:py-2 w-full text-sm bg-white dark:bg-ink-800 outline-none"
       type="text"
       placeholder={placeholder}
       value={value}
