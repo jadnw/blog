dbda512bdde18a1c080e7fea0ff0b17385356fad
jadnw <jadenwu137@protonmail.com>
2022-05-25 21:33:07 +0700

    :iphone: Responsive Typography

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -5,7 +5,9 @@ interface TypographyProps {
 
 const Typography = ({ text, children }: TypographyProps) => (
   <div className="pb-4 flex items-end justify-between border-b border-ink-500">
-    <h1 className="font-heading text-4xl font-bold">{text}</h1>
+    <h1 className="font-heading text-2xl xs:text-3xl sm:text-4xl font-bold">
+      {text}
+    </h1>
     <div>{children ?? ' '}</div>
   </div>
 )
