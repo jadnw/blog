3fa945cad948e2dcfc02d687ed5f84b9614fd8b3
jadnw <jadenwu137@protonmail.com>
2022-05-25 09:58:25 +0700

    :lipstick: Add more gutters

--- src/components/AdjacentsNav.tsx
+++ src/components/AdjacentsNav.tsx
@@ -14,11 +14,11 @@ const AdjacentsNav = ({
   route,
   adjacents: { prev, next },
 }: AdjacentsNavProps) => (
-  <div className="mt-4 py-4 flex flex-col items-stretch space-y-2 border-t border-ink-400 text-ink-500 dark:text-ink-400">
+  <div className="mt-12 py-2 flex flex-col items-stretch space-y-2 border-t border-ink-400 text-ink-500 dark:text-ink-400">
     {prev && (
       <div className="flex items-center justify-between">
         <Link href={`/${route}/${prev.slug}`}>
-          <a className="flex items-center font-medium hover:text-ink-900 dark:hover:text-white duration-75">
+          <a className="flex items-center font-medium hover:underline hover:text-ink-900 dark:hover:text-white duration-75">
             <ArrowLeft className="w-4 h-4" />
             <span className="ml-2">{prev.title}</span>
           </a>
@@ -30,7 +30,7 @@ const AdjacentsNav = ({
       <div className="flex items-center justify-between">
         <span className="text-ink-400 text-sm">{next.readingTime}</span>
         <Link href={`/${route}/${next.slug}`}>
-          <a className="flex items-center font-medium hover:text-ink-900 dark:hover:text-white duration-75">
+          <a className="flex items-center font-medium hover:underline hover:text-ink-900 dark:hover:text-white duration-75">
             <span className="mr-2">{next.title}</span>
             <ArrowRight className="w-4 h-4" />
           </a>
