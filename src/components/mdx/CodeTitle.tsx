1e61025299c32fd93b4300401cfedc707e503061
jadnw <jadenwu137@protonmail.com>
2022-05-25 21:18:33 +0700

    :lipstick: Fix gutters

--- src/components/mdx/CodeTitle.tsx
+++ src/components/mdx/CodeTitle.tsx
@@ -2,7 +2,7 @@ import DevIcon from '@/components/DevIcon'
 
 const CodeTitle = ({ ...props }) => (
   <div
-    className={`${props.className} mt-4 -mb-4 flex items-baseline space-x-2 italic`}
+    className={`${props.className} mt-4 -mb-4 flex items-baseline space-x-1 italic`}
   >
     <DevIcon filename={props.children} colored />
     <span>{props.children}</span>
