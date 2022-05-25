3cd60ad7a9b7eddba7df098f0081df96076abc23
jadnw <jadenwu137@protonmail.com>
2022-05-25 18:35:05 +0700

    :lipstick: Add devicon before filename in MDX renderer

--- src/components/mdx/CodeTitle.tsx
+++ src/components/mdx/CodeTitle.tsx
@@ -1,8 +1,10 @@
+import DevIcon from '@/components/DevIcon'
+
 const CodeTitle = ({ ...props }) => (
   <div
     className={`${props.className} mt-4 -mb-4 flex items-baseline space-x-2 italic`}
   >
-    <i className="devicon-typescript-plain colored"></i>
+    <DevIcon filename={props.children} colored />
     <span>{props.children}</span>
   </div>
 )
