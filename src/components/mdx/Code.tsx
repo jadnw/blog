4604571ba3ff2987fcdbe97247269c22a26c602d
jadnw <jadenwu137@protonmail.com>
2022-05-26 22:18:18 +0700

    :lipstick: Change inline code style

--- src/components/mdx/Code.tsx
+++ src/components/mdx/Code.tsx
@@ -1,6 +1,6 @@
 const Code = ({ ...props }) => (
   <code
-    className="text-primary-500 dark:text-primary-400 font-medium"
+    className="px-2 py-0.5 rounded-sm font-medium bg-ink-100 dark:bg-pre text-ink-700 dark:text-ink-100"
     {...props}
   />
 )
