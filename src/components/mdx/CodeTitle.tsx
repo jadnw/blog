3e603a3afba3c37005a557c78fcc69ae255ec87c
jadnw <jadenwu137@protonmail.com>
2022-05-26 21:22:11 +0700

    :lipstick: Place code title inside pre

--- src/components/mdx/CodeTitle.tsx
+++ src/components/mdx/CodeTitle.tsx
@@ -2,7 +2,7 @@ import DevIcon from '@/components/DevIcon'
 
 const CodeTitle = ({ ...props }) => (
   <div
-    className={`${props.className} mt-4 -mb-4 flex items-baseline space-x-1 italic`}
+    className={`${props.className} relative mt-4 -mb-5 px-4 py-1 flex items-baseline bg-pre text-sm font-medium text-ink-300 space-x-1 italic rounded-tl rounded-tr border-b border-ink-600 select-none z-10`}
   >
     <DevIcon filename={props.children} colored />
     <span>{props.children}</span>
