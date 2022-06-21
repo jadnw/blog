4739c57463a7e74a186650ae9af6dbf58e8d01f0
jadnw <jadenwu137@protonmail.com>
2022-06-21 13:22:17 +0700

    :lipstick: Change button appearance

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,14 +11,14 @@ const Button = ({ handler, href, text }: ButtonProps) =>
     <Link href={href}>
       <a
         data-content={text}
-        className="relative px-8 py-2 font-semibold  bg-ink-600 dark:bg-ink-100 rounded font-cursive after:content-[attr(data-content)] after:absolute after:top-0 after:left-0 after:translate-x-1 after:-translate-y-1 hover:after:translate-x-0 hover:after:translate-y-0 after:w-full after:h-full after:flex after:items-center after:justify-center after:rounded dark:after:bg-ink-800 after:bg-white after:border-2 after:border-ink-600 dark:after:border-ink-200 after:z-10 after:duration-75"
+        className="relative px-8 py-2 font-semibold bg-ink-600 dark:bg-ink-100 rounded font-cursive after:content-[attr(data-content)] after:absolute after:top-0 after:left-0 after:-translate-y-1 hover:after:translate-x-0 hover:after:translate-y-0 after:w-full after:h-full after:flex after:items-center after:justify-center after:rounded dark:after:bg-ink-800 after:bg-white after:border-2 after:border-ink-600 dark:after:border-ink-200 after:z-10 after:duration-75"
       >
         {text}
       </a>
     </Link>
   ) : (
     <button
-      className="relative px-8 py-2 font-semibold  bg-ink-600 dark:bg-ink-100 rounded font-cursive after:content-[attr(data-content)] after:absolute after:top-0 after:left-0 after:translate-x-1 after:-translate-y-1 hover:after:translate-x-0 hover:after:translate-y-0 after:w-full after:h-full after:flex after:items-center after:justify-center after:rounded dark:after:bg-ink-800 after:bg-white after:border-2 after:border-ink-600 dark:after:border-ink-200 after:z-10 after:duration-75"
+      className="relative px-8 py-2 font-semibold bg-ink-600 dark:bg-ink-100 rounded font-cursive after:content-[attr(data-content)] after:absolute after:top-0 after:left-0 after:-translate-y-1 hover:after:translate-x-0 hover:after:translate-y-0 after:w-full after:h-full after:flex after:items-center after:justify-center after:rounded dark:after:bg-ink-800 after:bg-white after:border-2 after:border-ink-600 dark:after:border-ink-200 after:z-10 after:duration-75"
       onClick={handler}
     >
       {text}
