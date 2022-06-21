abd3e7676a4450c4d6a0760841359b2ec8941c4d
jadnw <jadenwu137@protonmail.com>
2022-06-21 14:05:52 +0700

    :sparkles: Add a link button to navigate to its heading

--- src/components/mdx/Typo.tsx
+++ src/components/mdx/Typo.tsx
@@ -1,3 +1,7 @@
+import React from 'react'
+import LinkIcon from '@/assets/svgr/link.svg'
+import { useHover } from '@/hooks/index'
+
 const headingFontSizes = {
   h1: 'text-2xl md:text-3xl',
   h2: 'text-xl md:text-2xl',
@@ -6,13 +10,30 @@ const headingFontSizes = {
 
 interface TypoProps {
   as: 'h1' | 'h2' | 'h3'
+  id: string
+  children: React.ReactNode
 }
 
-const Typo = ({ as, ...props }: TypoProps) => (
-  <h1
-    className={`z-10 mt-8 mb-2 font-bold ${headingFontSizes[as]} scroll-mt-4`}
-    {...props}
-  />
-)
+const Typo = ({ as, id, children }: TypoProps) => {
+  const [hoverRef, isHovered] = useHover<HTMLHeadingElement>()
+
+  return (
+    <h1
+      ref={hoverRef}
+      className={`relative mt-8 mb-2 font-bold ${headingFontSizes[as]} scroll-mt-4`}
+      id={id}
+    >
+      {isHovered && (
+        <a
+          href={`#${id}`}
+          className="absolute top-1/2 -left-12 -translate-y-1/2 p-4"
+        >
+          <LinkIcon className="w-4 h-4" />
+        </a>
+      )}
+      {React.Children.toArray(children)[1]}
+    </h1>
+  )
+}
 
 export default Typo
